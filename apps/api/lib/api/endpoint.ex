96ceaa4e72508abd40c39d1bc41eb7c98c756952
Gabriel Poça <gabrielpoca@gmail.com>
2018-07-17 23:58:42 +0100

    add cors in dev

--- apps/api/lib/api/endpoint.ex
+++ apps/api/lib/api/endpoint.ex
@@ -18,6 +18,10 @@ defmodule Api.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
+  if Mix.env() == :dev do
+    plug CORSPlug
+  end
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
