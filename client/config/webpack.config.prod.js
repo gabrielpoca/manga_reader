f5f59ec2463998b4281ba98a269d8bafcd969402
Gabriel Poça <gabrielpoca@gmail.com>
2018-08-11 11:16:27 +0100

    update service worker configuration

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -1,42 +1,42 @@
-'use strict'
+'use strict';
 
-const autoprefixer = require('autoprefixer')
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WebpackPwaManifest = require('webpack-pwa-manifest')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const ManifestPlugin = require('webpack-manifest-plugin')
-const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin')
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
-const eslintFormatter = require('react-dev-utils/eslintFormatter')
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
-const paths = require('./paths')
-const getClientEnvironment = require('./env')
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const paths = require('./paths');
+const getClientEnvironment = require('./env');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-const publicPath = paths.servedPath
+const publicPath = paths.servedPath;
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
-const shouldUseRelativeAssetPaths = publicPath === './'
+const shouldUseRelativeAssetPaths = publicPath === './';
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
-const publicUrl = publicPath.slice(0, -1)
+const publicUrl = publicPath.slice(0, -1);
 // Get environment variables to inject into our app.
-const env = getClientEnvironment(publicUrl)
+const env = getClientEnvironment(publicUrl);
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
 if (env.stringified['process.env'].NODE_ENV !== '"production"') {
-  throw new Error('Production builds must have NODE_ENV=production.')
+  throw new Error('Production builds must have NODE_ENV=production.');
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css'
+const cssFilename = 'static/css/[name].[contenthash:8].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -45,7 +45,7 @@ const cssFilename = 'static/css/[name].[contenthash:8].css'
 const extractTextPluginOptions = shouldUseRelativeAssetPaths
   ? // Making sure that the publicPath goes back to to build folder.
     { publicPath: Array(cssFilename.split('/').length).join('../') }
-  : {}
+  : {};
 
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
@@ -310,14 +310,14 @@ module.exports = {
       logger(message) {
         if (message.indexOf('Total precache size is') === 0) {
           // This message occurs for every build and is a bit too noisy.
-          return
+          return;
         }
         if (message.indexOf('Skipping static resource') === 0) {
           // This message obscures real errors so we ignore it.
           // https://github.com/facebookincubator/create-react-app/issues/2612
-          return
+          return;
         }
-        console.log(message)
+        console.log(message);
       },
       minify: true,
       // For unknown URLs, fallback to the index page
@@ -329,56 +329,27 @@ module.exports = {
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
       runtimeCaching: [
         {
-          urlPattern: /https?[^.]+\.mangareader\.net\/(?!cover).*\.(jpg|png)/,
-          handler: 'cacheFirst',
+          urlPattern: /^https?:\/\/[^.]+.mangareader\.net\/.*\.(jpg|png)?/,
           options: {
             cache: {
               maxEntries: 200,
               name: 'image-cache'
             }
-          }
-        },
-        {
-          urlPattern: /^https?:\/\/[^.]+.mangareader\.net\/cover\/.*\.(jpg|png)?/,
-          handler: 'cacheFirst',
-          options: {
-            cache: {
-              maxEntries: 100,
-              name: 'cover-cache'
-            }
-          }
-        },
-        {
-          urlPattern: /^https?:\/\/[^.]+.mangareader\.net\/.*\.(jpg|png)?/,
+          },
           handler: function(request) {
-            return caches.match(request).catch(function() {
-              return fetch(request)
-            })
-          }
-        },
-        {
-          urlPattern: /https?.*\/api\/mangas\/[^\/]+\/chapters\/.*/,
-          handler: 'cacheFirst',
-          options: {
-            cache: {
-              maxEntries: 100,
-              name: 'chapter-cache'
-            }
-          }
-        },
-        {
-          urlPattern: /https?.*\/api\/mangas\/[^\/]+/,
-          handler: 'networkFirst',
-          options: {
-            cache: {
-              maxEntries: 50,
-              name: 'manga-cache'
-            }
+            return caches
+              .match(request)
+              .catch(function() {
+                return fetch(request);
+              })
+              .then(function(response) {
+                return response || fetch(request);
+              });
           }
         },
         {
           urlPattern: /https?.*placeholder.jpg/,
-          handler: 'cacheFirst',
+          handler: 'fastest',
           options: {
             cache: {
               maxEntries: 10,
@@ -393,23 +364,32 @@ module.exports = {
         {
           urlPattern: /https?.*/,
           handler: function(request) {
-            if (
-              request.url.includes('/api') ||
-              request.url.match(/\.(js|json|css|png|jpg)$/)
-            ) {
-              return caches.match(request).catch(function() {
-                return fetch(request)
-              })
+            if (request.url.includes('/api')) {
+              return fetch(request);
+            }
+
+            if (request.url.match(/\.(js|json|css|png|jpg)$/)) {
+              return caches
+                .match(request)
+                .catch(function() {
+                  return fetch(request);
+                })
+                .then(function(response) {
+                  return response || fetch(request);
+                });
             }
 
             return caches
               .match(request)
-              .catch(function() {
-                return caches.match('/')
+              .then(function(response) {
+                return response || caches.match('/');
+              })
+              .then(function(response) {
+                return response || fetch(request);
               })
               .catch(function() {
-                return fetch(request)
-              })
+                return fetch(request);
+              });
           }
         }
       ]
@@ -430,4 +410,4 @@ module.exports = {
     tls: 'empty',
     child_process: 'empty'
   }
-}
+};
